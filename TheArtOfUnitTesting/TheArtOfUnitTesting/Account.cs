407f264898942f32d16c68fbd700e6ebcc61a148
Djelekar <Stefan.Djelekar@zuehlke.com>
2017-10-16 22:41:42 +0200

    created project

---
+++ TheArtOfUnitTesting/TheArtOfUnitTesting/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheArtOfUnitTesting
+{
+    public class Account
+    {
+        private int _balance;
+
+        public Account()
+        {
+            _balance = 0;
+        }
+
+        public Account(int balance)
+        {
+            _balance = balance;
+        }
+
+        public int Deposit(int amount)
+        {
+            return _balance += amount;
+        }
+    }
+}
---
+++ TheArtOfUnitTesting/TheArtOfUnitTesting_Test/AccountTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using TheArtOfUnitTesting;
+
+namespace TheArtOfUnitTesting_Test
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        // bad
+        [Test]
+        public void InitialBalance_Deposit200_BalanceIs200_Bad()
+        {
+            var account = new Account();
+
+            Assert.AreEqual(200, account.Deposit(200));
+        }
+
+        // better
+        [Test]
+        public void InitialBalance_Deposit200_BalanceIs200_Better()
+        {
+            var account = new Account();
+
+            var actualBalance = account.Deposit(200);
+
+            Assert.AreEqual(200, actualBalance);
+        }
+
+        // good
+        [TestCase(100, 200, 300)]
+        [TestCase(200, 300, 500)]
+        public void InitialBalance_Deposit_NewBalanceCorrect_Good(int initialBalance, int deposit, int expectedBalance)
+        {
+            var account = new Account(initialBalance);
+
+            var actualBalance = account.Deposit(deposit);
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+    }
+}
